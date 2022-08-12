b7b5e4fd09426f75aeab2c93f973388780badd57
kyoshong <qkrgytjd0827@naver.com>
2022-08-12 12:20:13 +0900

    :bug: fix: set mariadb root password

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,8 @@
 service mysql start;
 
+echo "USE mysql;" | mysql;
+
+echo "set password=password('$DB_ROOT_PASSWORD');" | mysql;
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql;
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql;
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" | mysql;
